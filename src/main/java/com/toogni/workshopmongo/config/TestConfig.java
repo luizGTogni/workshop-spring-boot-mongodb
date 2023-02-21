a03bb8a4770556768e3c43b5ae54f21988341d04
Luiz Togni <contato_togni@outlook.com.br>
2023-02-20 23:01:51 -0300

    feat: Creating entity Post with nested User

--- src/main/java/com/toogni/workshopmongo/config/TestConfig.java
+++ src/main/java/com/toogni/workshopmongo/config/TestConfig.java
@@ -1,12 +1,16 @@
 package com.toogni.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.toogni.workshopmongo.entities.Post;
 import com.toogni.workshopmongo.entities.User;
+import com.toogni.workshopmongo.repositories.PostRepository;
 import com.toogni.workshopmongo.repositories.UserRepository;
 
 @Configuration
@@ -15,14 +19,26 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User user2 = new User(null, "Alex Green", "alex@gmail.com");
 		User user3 = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", user1);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", user1);
+		
 		userRepository.saveAll(Arrays.asList(user1, user2, user3));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
